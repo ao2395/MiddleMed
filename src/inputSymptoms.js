aae554cdcf62bf708963ca17d98bf4f25a7b4fb2
ao2395 <ao2395@nyu.edu>
2024-10-24 16:18:17 -0400

    css

--- src/inputSymptoms.js
+++ src/inputSymptoms.js
@@ -8,11 +8,15 @@ function SymptomAndDiagnosis() {
     const [age, setAge] = useState('');
     const [gender, setGender] = useState('');
     const [diagnosis, setDiagnosis] = useState([]);
+    const [useLiveAPI, setUseLiveAPI] = useState(false);  // State to toggle between live and sandbox
 
-    const token = process.env.REACT_APP_TOKEN;;
-    console.log(token);
+    const sandboxToken = process.env.REACT_APP_SANDBOX_TOKEN;
+    console.log(sandboxToken)
+    const liveToken = process.env.REACT_APP_LIVE_TOKEN;
+    const apiBaseURL = useLiveAPI ? "https://healthservice.priaid.ch" : "https://sandbox-healthservice.priaid.ch";
+    const token = useLiveAPI ? liveToken : sandboxToken;
     useEffect(() => {
-        fetch(`https://sandbox-healthservice.priaid.ch/symptoms?token=${token}&format=json&language=en-gb`)
+        fetch(`${apiBaseURL}/symptoms?token=${token}&format=json&language=en-gb`)
             .then(response => response.json())
             .then(data => {
                 setSymptomsList(data);
@@ -23,7 +27,7 @@ function SymptomAndDiagnosis() {
                 setCheckedState(initialState);
             })
             .catch(error => console.error('Error fetching symptoms:', error));
-    }, []);
+    }, [token, apiBaseURL]);
 
     const handleCheckboxChange = (id) => {
         setCheckedState(prevState => ({
@@ -38,7 +42,7 @@ function SymptomAndDiagnosis() {
             .map(([key]) => key);
 
         if (selectedSymptoms.length > 0) {
-            fetch(`https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[${selectedSymptoms}]&gender=${gender}&year_of_birth=${age}&token=${token}&format=json&language=en-gb`)
+            fetch(`${apiBaseURL}/diagnosis?symptoms=[${selectedSymptoms}]&gender=${gender}&year_of_birth=${age}&token=${token}&format=json&language=en-gb`)
                 .then(response => response.json())
                 .then(data => {
                     setDiagnosis(data);
@@ -51,59 +55,72 @@ function SymptomAndDiagnosis() {
 
     return (
         <div className="container">
-            <div className="input-group">
-                <input
-                    type="text"
-                    className="search-input"
-                    value={search}
-                    onChange={e => setSearch(e.target.value)}
-                    placeholder="Search symptoms"
-                />
-                <div className="symptom-checkboxes">
-                    {symptomsList.filter(symptom => symptom.Name.toLowerCase().includes(search.toLowerCase())).map(symptom => (
-                        <label key={symptom.ID} className="checkbox-container">
-                            {symptom.Name}
-                            <input
-                                type="checkbox"
-                                checked={checkedState[symptom.ID]}
-                                onChange={() => handleCheckboxChange(symptom.ID)}
-                            />
-                            <span className="checkmark"></span>
-                        </label>
-                    ))}
+            <header className="header">
+                <div className="header-content">
+                    <h1>MiddleMed</h1>
+                    <label className="switch">
+                        <input type="checkbox" checked={useLiveAPI} onChange={() => setUseLiveAPI(!useLiveAPI)} />
+                        <span className="slider round"></span>
+                    </label>
                 </div>
-                <input
-                    type="number"
-                    className="input-text"
-                    value={age}
-                    onChange={e => setAge(e.target.value)}
-                    placeholder="Enter your birth year"
-                />
-                <select
-                    className="input-select"
-                    value={gender}
-                    onChange={e => setGender(e.target.value)}
-                >
-                    <option value="">Select Gender</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
-                <button className="submit-button" onClick={handleDiagnosis}>Diagnose</button>
+            </header>
+            <div className="body">
+                <div className="left-column">
+                    <input
+                        type="number"
+                        className="input-text"
+                        value={age}
+                        onChange={e => setAge(e.target.value)}
+                        placeholder="Enter your age"
+                    />
+                    <select
+                        className="input-select"
+                        value={gender}
+                        onChange={e => setGender(e.target.value)}
+                    >
+                        <option value="">Select Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                    <button className="submit-button" onClick={handleDiagnosis}>Diagnose</button>
+                    <input
+                        type="text"
+                        className="search-input"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                        placeholder="Search symptoms"
+                    />
+                    <div className="symptom-checkboxes">
+                        {symptomsList.filter(symptom => symptom.Name.toLowerCase().includes(search.toLowerCase())).map(symptom => (
+                            <label key={symptom.ID} className="checkbox-container">
+                                {symptom.Name}
+                                <input
+                                    type="checkbox"
+                                    checked={checkedState[symptom.ID]}
+                                    onChange={() => handleCheckboxChange(symptom.ID)}
+                                />
+                                <span className="checkmark"></span>
+                            </label>
+                        ))}
+                    </div>
+                </div>
+                <div className="right-column">
+                    {diagnosis.length > 0 && (
+                        <ul className="diagnosis-list">
+                            {diagnosis.map((diag, index) => (
+                                <li key={index}>
+                                    <strong>{diag.Issue.Name}</strong> (Accuracy: {diag.Issue.Accuracy}%)
+                                    <ul>
+                                        {diag.Specialisation.map((spec, specIndex) => (
+                                            <li key={specIndex}>{spec.Name}</li>
+                                        ))}
+                                    </ul>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
             </div>
-            {diagnosis.length > 0 && (
-                <ul className="diagnosis-list">
-                    {diagnosis.map((diag, index) => (
-                        <li key={index}>
-                            <strong>{diag.Issue.Name}</strong> (Accuracy: {diag.Issue.Accuracy}%)
-                            <ul>
-                                {diag.Specialisation.map((spec, specIndex) => (
-                                    <li key={specIndex}>{spec.Name}</li>
-                                ))}
-                            </ul>
-                        </li>
-                    ))}
-                </ul>
-            )}
         </div>
     );
 }
